7a315f5b4d81981f8fe7a946a4991dae72ad092a
ppavia <ppavia@protonmail.com>
2020-12-01 22:22:39 +0100

    cleanup staged area

--- src/main/java/ppa/spring/petclinic/model/Owner.java
+++
@@ -1,4 +0,0 @@
-package ppa.spring.petclinic.model;
-
-public class Owner extends Person {
-}
--- src/main/java/ppa/spring/petclinic/model/PetType.java
+++
@@ -1,6 +0,0 @@
-package ppa.spring.petclinic.model;
-
-public class PetType {
-
-    private String name;
-}
