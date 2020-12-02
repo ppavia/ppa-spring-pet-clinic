00c5fbefa9ee82fc301953c9ba803a385758df88
ppavia <ppavia@protonmail.com>
2020-12-02 09:21:09 +0100

    add assertion to junit test

--- pet-clinic-web/src/test/java/petclinic/PpaSpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PpaSpringPetClinicApplicationTests.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import petclinic.web.PpaPetClinicApplication;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest(classes = PpaPetClinicApplication.class)
 class PpaSpringPetClinicApplicationTests {
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 }
